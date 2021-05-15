858f6b4628cc1bbfd952c22d5ed863b671fae21f
ZekicanBudin <44768735+ZekicanBudin@users.noreply.github.com>
2021-05-15 16:23:34 +0300

    Initial Version

---
+++ src/main/java/com/cng457/webservice/Cng457P2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cng457.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cng457P2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cng457P2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cng457/webservice/Cng457P2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cng457.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cng457P2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
